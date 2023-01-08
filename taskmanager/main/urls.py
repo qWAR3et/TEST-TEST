91e05841cd014f1d611123fceffdf24b3f600691
Alex <kifir.face@gmail.com>
2023-01-08 16:26:05 +0300

    gl

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -1,7 +1,26 @@
 
 from django.urls import path
+from django.urls import include
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('index_2', views.index_2),
+    path('index_3', views.index_3),
+    path('index_4', views.index_4),
+    path('error', views.error),
+    path('about', views.about),
+    path('blog', views.blog),
+    path('cart', views.cart),
+    path('checkout', views.checkout),
+    path('compare', views.compare),
+    path('contact', views.contact),
+    path('forgot-password', views.forgot_password),
+    path('login', views.login),
+    path('product', views.product),
+    path('register', views.register),
+    path('shop', views.shop),
+    path('single-blog', views.single_blog),
+    path('terms-conditions', views.terms_conditions),
+    path('wishlist', views.wishlist),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,5 +1,78 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def index_2(request):
+    return render(request, 'main/index_2.html')
+
+
+def index_3(request):
+    return render(request, 'main/index_3.html')
+
+
+def index_4(request):
+    return render(request, 'main/index_4.html')
+
+
+def error(request):
+    return render(request, 'main/error.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+
+def cart(request):
+    return render(request, 'main/cart.html')
+
+
+def checkout(request):
+    return render(request, 'main/checkout.html')
+
+
+def compare(request):
+    return render(request, 'main/compare.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+
+def forgot_password(request):
+    return render(request, 'main/forgot_password.html')
+
+
+def login(request):
+    return render(request, 'main/login.html')
+
+
+def product(request):
+    return render(request, 'main/product.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
+
+
+def shop(request):
+    return render(request, 'main/shop.html')
+
+
+def single_blog(request):
+    return render(request, 'main/single_blog.html')
+
+
+def terms_conditions(request):
+    return render(request, 'main/terms_conditions.html')
+
+
+def wishlist(request):
+    return render(request, 'main/wishlist.html')
